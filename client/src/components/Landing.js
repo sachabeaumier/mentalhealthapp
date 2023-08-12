67a5f9779b9da8ef0f929249659d5bf9917a61c6
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-08-13 10:16:04 +1200

    deployment

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Landing = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Landing
---
+++ client/src/components/Questionnaire.js
@@ -0,0 +1,9 @@
+const Questionnaire = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Questionnaire
